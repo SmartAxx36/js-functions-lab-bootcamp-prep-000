483b123710cb34194c8732d6eb1498bfc3a26021
Gavin Moore <gavizoid16@gmail.com>
2018-08-17 04:35:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function holidayCountdown(holiday, days) {
   return ("It's 20 days until Mother's Day!") 
 }
 function happyHolidayTo(holiday, name) {
-  return ("Happy Independence day, you!")
+  return ("Happy Independence Day, you!")
 }
